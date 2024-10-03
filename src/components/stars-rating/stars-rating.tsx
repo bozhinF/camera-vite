4fad1f04ab6e29ca4867b00e2e5bea29f0a3a1f5
bozhinF <bojin.fedor@gmail.com>
2024-10-03 20:28:50 +0300

    Добавит компонент StarsRating

---
+++ src/components/stars-rating/stars-rating.tsx
@@ -0,0 +1,27 @@
+const RATING_STARS_COUNT = 5;
+type StarsRatingProps = {
+  rating: number;
+  reviewCount: number;
+};
+function StarsRating({ rating, reviewCount }: StarsRatingProps): JSX.Element {
+  return (
+    <div className="rate product-card__rate">
+      {Array.from({ length: RATING_STARS_COUNT }, (_, i) => i + 1).map(
+        (number) => (
+          <svg key={number} width={17} height={16} aria-hidden="true">
+            <use
+              xlinkHref={number > rating ? '#icon-star' : '#icon-full-star'}
+            />
+          </svg>
+        )
+      )}
+      <p className="visually-hidden">Рейтинг: {rating}</p>
+      <p className="rate__count">
+        <span className="visually-hidden">Всего оценок:</span>
+        {reviewCount}
+      </p>
+    </div>
+  );
+}
+
+export default StarsRating;
