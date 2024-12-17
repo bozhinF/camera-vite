49247e1bc542ee809da95e486ba44234d56e8969
bozhinF <bojin.fedor@gmail.com>
2024-12-17 19:31:59 +0300

    Добавит тесты для селекторов productsSlice

---
+++ src/store/products-slice/selectors.test.ts
@@ -0,0 +1,67 @@
+import { NameSpace, RequestStatus } from '../../const/const';
+import { getMockProduct, getMockReview } from '../../util/util';
+import {
+  getAllProducts,
+  getAllProductsStatus,
+  getBasket,
+  getPostOrderStatus,
+  getProductDetails,
+  getProductDetailsStatus,
+  getProductReviews,
+} from './selectors';
+
+describe('ProductSlice selectors', () => {
+  const products = Array.from({ length: 5 }, () => getMockProduct());
+  const product = products[0];
+  const reviews = Array.from({ length: 5 }, () => getMockReview());
+  const basket = products.map((item) => item.id);
+  const state = {
+    [NameSpace.Products]: {
+      allProductsStatus: RequestStatus.Idle,
+      allProducts: products,
+      productDetailsStatus: RequestStatus.Idle,
+      productDetails: product,
+      productReviewsStatus: RequestStatus.Idle,
+      productReviews: reviews,
+      basket: basket,
+      postOrderStatus: RequestStatus.Idle,
+    },
+  };
+  it('should return allProducts', () => {
+    const result = getAllProducts(state);
+    expect(result).toEqual(products);
+  });
+
+  it('should return allProductsStatus', () => {
+    const expectedStatus = state[NameSpace.Products].allProductsStatus;
+    const result = getAllProductsStatus(state);
+    expect(result).toBe(expectedStatus);
+  });
+
+  it('should return allProductsDetails', () => {
+    const result = getProductDetails(state);
+    expect(result).toEqual(product);
+  });
+
+  it('should return productDetailsStatus', () => {
+    const expectedStatus = state[NameSpace.Products].productDetailsStatus;
+    const result = getProductDetailsStatus(state);
+    expect(result).toBe(expectedStatus);
+  });
+
+  it('should return productReviews', () => {
+    const result = getProductReviews(state);
+    expect(result).toEqual(reviews);
+  });
+
+  it('should return postOrderStatus', () => {
+    const expectedStatus = state[NameSpace.Products].postOrderStatus;
+    const result = getPostOrderStatus(state);
+    expect(result).toBe(expectedStatus);
+  });
+
+  it('should return basket', () => {
+    const result = getBasket(state);
+    expect(result).toEqual(basket);
+  });
+});
