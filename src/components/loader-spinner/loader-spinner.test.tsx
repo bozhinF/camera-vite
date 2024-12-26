b12fc8cb69e4e7e54eb9431fe3b10d4bbb39bf76
bozhinF <bojin.fedor@gmail.com>
2024-12-26 22:09:00 +0300

    Добавит тесты для компонента LoaderSpinner

---
+++ src/components/loader-spinner/loader-spinner.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import css from './loader-spinner.module.css';
+import LoaderSpinner from './loader-spinner';
+
+describe('Component: LoaderSpinner', () => {
+  test('shoul renders the loader spinner correctly', () => {
+    render(<LoaderSpinner />);
+
+    const loader = screen.getByTestId('loader');
+    expect(loader).toBeInTheDocument();
+    expect(loader).toHaveClass(css.loader);
+  });
+});
--- src/components/loader-spinner/loader-spinner.tsx
+++ src/components/loader-spinner/loader-spinner.tsx
@@ -1,7 +1,7 @@
 import css from './loader-spinner.module.css';
 
 function LoaderSpinner(): JSX.Element {
-  return <div className={css.loader}></div>;
+  return <div className={css.loader} data-testid="loader"></div>;
 }
 
 export default LoaderSpinner;
