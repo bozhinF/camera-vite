83d74653f72b0b0358f92dbd11557b36af2a4921
bozhinF <bojin.fedor@gmail.com>
2024-12-28 23:53:26 +0300

    Поправит тесты для компонента BasketItem

--- src/components/basket-item/basket-item.test.tsx
+++ src/components/basket-item/basket-item.test.tsx
@@ -1,5 +1,9 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { getMockProduct, getMockStore } from '../../util/mocks';
+import {
+  getMockProduct,
+  getMockProductsState,
+  getMockStore,
+} from '../../util/mocks';
 import { rootReducer } from '../../store/root-reducer';
 import { createStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
@@ -10,22 +14,13 @@ import {
 } from '../../store/products-slice/products-slice';
 import userEvent from '@testing-library/user-event';
 import BasketItem from './basket-item';
-import { NameSpace, RequestStatus } from '../../const/const';
+import { ElementRole, NameSpace } from '../../const/const';
+import { State } from '../../types/state';
 
 describe('Component: BasketItem', () => {
-  const initialProductsState = {
-    allProductsStatus: RequestStatus.Idle,
-    allProducts: [],
-    productDetailsStatus: RequestStatus.Idle,
-    productDetails: null,
-    productReviewsStatus: RequestStatus.Idle,
-    productReviews: [],
-    basket: [],
-    postOrderStatus: RequestStatus.Idle,
-  };
   const onCloseButtonClick = vi.fn();
 
-  const createMockStore = (initialState: ReturnType<typeof getMockStore>) =>
+  const createMockStore = (initialState: State) =>
     createStore(rootReducer, initialState);
 
   beforeEach(() => {
@@ -34,13 +29,11 @@ describe('Component: BasketItem', () => {
 
   it('should renders correctly', () => {
     const product = getMockProduct();
-    const productsState = {
-      ...initialProductsState,
+    const productsState = getMockProductsState({
       basket: [product.id, product.id],
-    };
+    });
     const mockStore = getMockStore({ [NameSpace.Products]: productsState });
     const store = createMockStore(mockStore);
-
     const productName = product.name;
     const productVendorCode = product.vendorCode;
     const productCategory = `${product.type} ${
@@ -52,6 +45,8 @@ describe('Component: BasketItem', () => {
     const prevButtonLabel = /уменьшить количество товара/i;
     const nextButtonLabel = /увеличить количество товара/i;
     const closeButtonLabel = /Удалить товар/i;
+    const quantityInputTestId = 'quantity';
+    const expectedQuantityInputValue = '2';
 
     render(
       <Provider store={store}>
@@ -63,21 +58,22 @@ describe('Component: BasketItem', () => {
       </Provider>
     );
 
-    const quantityInput: HTMLInputElement = screen.getByTestId('quantity');
+    const quantityInput: HTMLInputElement =
+      screen.getByTestId(quantityInputTestId);
     expect(screen.getByText(productName)).toBeInTheDocument();
     expect(screen.getByText(productVendorCode)).toBeInTheDocument();
     expect(screen.getByText(productCategory)).toBeInTheDocument();
     expect(screen.getByText(productLevel)).toBeInTheDocument();
     expect(
-      screen.getByRole('button', { name: prevButtonLabel })
+      screen.getByRole(ElementRole.Button, { name: prevButtonLabel })
     ).toBeInTheDocument();
     expect(
-      screen.getByRole('button', { name: nextButtonLabel })
+      screen.getByRole(ElementRole.Button, { name: nextButtonLabel })
     ).toBeInTheDocument();
     expect(quantityInput).toBeInTheDocument();
-    expect(quantityInput.value).toBe('2');
+    expect(quantityInput.value).toBe(expectedQuantityInputValue);
     expect(
-      screen.getByRole('button', { name: closeButtonLabel })
+      screen.getByRole(ElementRole.Button, { name: closeButtonLabel })
     ).toBeInTheDocument();
   });
 
@@ -102,15 +98,14 @@ describe('Component: BasketItem', () => {
 
   it('should dispatches removeItemFromBasket when prev button is clicked', () => {
     const product = getMockProduct();
-    const productsState = {
-      ...initialProductsState,
+    const productsState = getMockProductsState({
       basket: [product.id, product.id],
-    };
+    });
     const mockStore = getMockStore({ [NameSpace.Products]: productsState });
     const store = createMockStore(mockStore);
     const dispatchSpy = vi.spyOn(store, 'dispatch');
-
     const prevButtonLabel = /уменьшить количество товара/i;
+    const expectedDispatchCalledTimes = 1;
 
     render(
       <Provider store={store}>
@@ -122,25 +117,24 @@ describe('Component: BasketItem', () => {
       </Provider>
     );
 
-    const prevButton = screen.getByRole('button', {
+    const prevButton = screen.getByRole(ElementRole.Button, {
       name: prevButtonLabel,
     });
     fireEvent.click(prevButton);
     expect(dispatchSpy).toHaveBeenCalledWith(removeItemFromBasket(product.id));
-    expect(dispatchSpy).toHaveBeenCalledTimes(1);
+    expect(dispatchSpy).toHaveBeenCalledTimes(expectedDispatchCalledTimes);
   });
 
   it('should dispatches addItemToBasket when next button is clicked', () => {
     const product = getMockProduct();
-    const productsState = {
-      ...initialProductsState,
+    const productsState = getMockProductsState({
       basket: [product.id, product.id],
-    };
+    });
     const mockStore = getMockStore({ [NameSpace.Products]: productsState });
     const store = createMockStore(mockStore);
     const dispatchSpy = vi.spyOn(store, 'dispatch');
-
     const nextButtonLabel = /увеличить количество товара/i;
+    const expectedDispatchCalledTimes = 1;
 
     render(
       <Provider store={store}>
@@ -152,23 +146,25 @@ describe('Component: BasketItem', () => {
       </Provider>
     );
 
-    const nextButton = screen.getByRole('button', {
+    const nextButton = screen.getByRole(ElementRole.Button, {
       name: nextButtonLabel,
     });
     fireEvent.click(nextButton);
     expect(dispatchSpy).toHaveBeenCalledWith(addItemToBasket(product.id));
-    expect(dispatchSpy).toHaveBeenCalledTimes(1);
+    expect(dispatchSpy).toHaveBeenCalledTimes(expectedDispatchCalledTimes);
   });
 
   it('should dispatches updateBasket when input quantity', async () => {
     const product = getMockProduct();
-    const productsState = {
-      ...initialProductsState,
+    const productsState = getMockProductsState({
       basket: [product.id, product.id],
-    };
+    });
     const mockStore = getMockStore({ [NameSpace.Products]: productsState });
     const store = createMockStore(mockStore);
     const dispatchSpy = vi.spyOn(store, 'dispatch');
+    const quantityInputTestId = 'quantity';
+    const expectedProductQuantity = 5;
+    const expectedDispatchCalledTimes = 1;
 
     render(
       <Provider store={store}>
@@ -180,23 +176,25 @@ describe('Component: BasketItem', () => {
       </Provider>
     );
 
-    const quantityInput: HTMLInputElement = screen.getByTestId('quantity');
+    const quantityInput: HTMLInputElement =
+      screen.getByTestId(quantityInputTestId);
     await userEvent.click(quantityInput);
-    await userEvent.keyboard('5');
-    const expectedBasket = Array.from({ length: 5 }, () => product.id);
+    await userEvent.keyboard(`${expectedProductQuantity}`);
+    const expectedBasket = Array.from(
+      { length: expectedProductQuantity },
+      () => product.id
+    );
     expect(dispatchSpy).toHaveBeenCalledWith(updateBasket(expectedBasket));
-    expect(dispatchSpy).toHaveBeenCalledTimes(1);
+    expect(dispatchSpy).toHaveBeenCalledTimes(expectedDispatchCalledTimes);
   });
 
   it('should disabled prev button when quantity equals 1', () => {
     const product = getMockProduct();
-    const productsState = {
-      ...initialProductsState,
-      basket: [product.id],
-    };
+    const productsState = getMockProductsState({
+      basket: [product.id, product.id],
+    });
     const mockStore = getMockStore({ [NameSpace.Products]: productsState });
     const store = createMockStore(mockStore);
-
     const prevButtonLabel = /уменьшить количество товара/i;
 
     render(
@@ -210,19 +208,17 @@ describe('Component: BasketItem', () => {
     );
 
     expect(
-      screen.getByRole('button', { name: prevButtonLabel })
+      screen.getByRole(ElementRole.Button, { name: prevButtonLabel })
     ).toBeDisabled();
   });
 
   it('should disabled next button when quantity equals 9', () => {
     const product = getMockProduct();
-    const productsState = {
-      ...initialProductsState,
+    const productsState = getMockProductsState({
       basket: Array.from({ length: 9 }, () => product.id),
-    };
+    });
     const mockStore = getMockStore({ [NameSpace.Products]: productsState });
     const store = createMockStore(mockStore);
-
     const nextButtonLabel = /увеличить количество товара/i;
 
     render(
@@ -236,7 +232,7 @@ describe('Component: BasketItem', () => {
     );
 
     expect(
-      screen.getByRole('button', { name: nextButtonLabel })
+      screen.getByRole(ElementRole.Button, { name: nextButtonLabel })
     ).toBeDisabled();
   });
 });
