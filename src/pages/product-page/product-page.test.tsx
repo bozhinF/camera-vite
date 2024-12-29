789655ea0fc04b224d1ff5a61ba10ff04009c702
bozhinF <bojin.fedor@gmail.com>
2024-12-29 22:53:52 +0300

    Поправит тесты для компонента ProductPage

--- src/pages/product-page/product-page.test.tsx
+++ src/pages/product-page/product-page.test.tsx
@@ -1,5 +1,12 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import { AppRoute, Crumb, NameSpace, RequestStatus } from '../../const/const';
+import {
+  AppRoute,
+  Crumb,
+  ElementAttribute,
+  ElementRole,
+  NameSpace,
+  RequestStatus,
+} from '../../const/const';
 import {
   createBrowserHistory,
   createMemoryHistory,
@@ -19,10 +26,11 @@ import { HelmetProvider } from 'react-helmet-async';
 import { setFilters } from '../../store/filter-slice/filter-slice';
 
 describe('Component: ProductPage', () => {
+  type WindowScrollTo = (options?: ScrollToOptions) => void;
   let mockHistory: MemoryHistory;
 
   beforeAll(() => {
-    window.scrollTo = vi.fn() as unknown as (options?: ScrollToOptions) => void;
+    window.scrollTo = vi.fn() as unknown as WindowScrollTo;
   });
 
   beforeEach(() => {
@@ -31,6 +39,7 @@ describe('Component: ProductPage', () => {
   });
 
   it('should renders breadcrumbs', () => {
+    const breadcrumbsTipTestId = 'breadcrumbs-tip';
     const mockProduct = getMockProduct();
     const expectedProductName = mockProduct.name;
     const mockStore = getMockStore({
@@ -45,17 +54,22 @@ describe('Component: ProductPage', () => {
 
     render(withStoreComponent);
 
-    expect(screen.getByRole('link', { name: Crumb.Main })).toBeInTheDocument();
     expect(
-      screen.getByRole('link', { name: Crumb.Catalog })
+      screen.getByRole(ElementRole.Link, { name: Crumb.Main })
     ).toBeInTheDocument();
-    expect(screen.getByTestId('breadcrumbs-tip')).toBeInTheDocument();
-    expect(screen.getByTestId('breadcrumbs-tip').textContent).toBe(
+    expect(
+      screen.getByRole(ElementRole.Link, { name: Crumb.Catalog })
+    ).toBeInTheDocument();
+    expect(screen.getByTestId(breadcrumbsTipTestId)).toBeInTheDocument();
+    expect(screen.getByTestId(breadcrumbsTipTestId).textContent).toBe(
       expectedProductName
     );
   });
 
   it('should renders product image', () => {
+    const pictureTestId = 'picture';
+    const sourceTestId = 'source';
+    const imageTestId = 'image';
     const mockProduct = getMockProduct();
     const mockStore = getMockStore({
       [NameSpace.Products]: getMockProductsState({
@@ -69,12 +83,24 @@ describe('Component: ProductPage', () => {
 
     render(withStoreComponent);
 
-    expect(screen.getByTestId('picture')).toBeInTheDocument();
-    expect(screen.getByTestId('source')).toBeInTheDocument();
-    expect(screen.getByTestId('image')).toBeInTheDocument();
+    expect(screen.getByTestId(pictureTestId)).toBeInTheDocument();
+    expect(screen.getByTestId(sourceTestId)).toBeInTheDocument();
+    expect(screen.getByTestId(imageTestId)).toBeInTheDocument();
   });
 
   it('should renders product details', () => {
+    const starTestId = 'star';
+    const expectedStarsCount = 5;
+    const starImageTestId = 'star-image';
+    const expectedFullStarXlinkHref = '#icon-full-star';
+    const expectedEmptyStarXlinkHref = '#icon-star';
+    const expectedCharacteristicsTabText = 'Характеристики';
+    const expectedDescriptionTabText = 'Описание';
+    const activeTabClass = 'is-active';
+    const expectedVendorCodeTitleText = 'Артикул:';
+    const expectedCategoryTitleText = 'Категория:';
+    const expectedTypeTitleText = 'Тип камеры:';
+    const expectedLevelTitleText = 'Уровень:';
     const mockProduct = getMockProduct();
     const expectedProductName = mockProduct.name;
     const expectedProductPrice = `${divideNumberByPieces(mockProduct.price)} ₽`;
@@ -91,35 +117,44 @@ describe('Component: ProductPage', () => {
     render(withStoreComponent);
 
     expect(
-      screen.getByRole('heading', { name: expectedProductName })
+      screen.getByRole(ElementRole.Heading, { name: expectedProductName })
     ).toBeInTheDocument();
-    expect(screen.getAllByTestId('star')).toHaveLength(5);
-    const starsImage = screen.getAllByTestId('star-image');
+    expect(screen.getAllByTestId(starTestId)).toHaveLength(expectedStarsCount);
+    const starsImage = screen.getAllByTestId(starImageTestId);
     starsImage.forEach((starImage, index) =>
-      expect(starImage.getAttribute('xlink:href')).toBe(
-        index < mockProduct.rating ? '#icon-full-star' : '#icon-star'
+      expect(starImage.getAttribute(ElementAttribute.XlinkHref)).toBe(
+        index < mockProduct.rating
+          ? expectedFullStarXlinkHref
+          : expectedEmptyStarXlinkHref
       )
     );
     expect(screen.getByText(expectedProductPrice)).toBeInTheDocument();
-    expect(screen.getByText('Характеристики')).toBeInTheDocument();
-    expect(screen.getByText('Описание')).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Характеристики' })).toHaveClass(
-      'is-active'
-    );
-    expect(screen.getByRole('button', { name: 'Описание' })).not.toHaveClass(
-      'is-active'
-    );
-    expect(screen.getByText('Артикул:')).toBeInTheDocument();
+    expect(
+      screen.getByText(expectedCharacteristicsTabText)
+    ).toBeInTheDocument();
+    expect(screen.getByText(expectedDescriptionTabText)).toBeInTheDocument();
+    expect(
+      screen.getByRole(ElementRole.Button, {
+        name: expectedCharacteristicsTabText,
+      })
+    ).toHaveClass(activeTabClass);
+    expect(
+      screen.getByRole(ElementRole.Button, { name: expectedDescriptionTabText })
+    ).not.toHaveClass(activeTabClass);
+    expect(screen.getByText(expectedVendorCodeTitleText)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.vendorCode)).toBeInTheDocument();
-    expect(screen.getByText('Категория:')).toBeInTheDocument();
+    expect(screen.getByText(expectedCategoryTitleText)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.category)).toBeInTheDocument();
-    expect(screen.getByText('Тип камеры:')).toBeInTheDocument();
+    expect(screen.getByText(expectedTypeTitleText)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.type)).toBeInTheDocument();
-    expect(screen.getByText('Уровень:')).toBeInTheDocument();
+    expect(screen.getByText(expectedLevelTitleText)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.level)).toBeInTheDocument();
   });
 
   it('should renders product reviews', () => {
+    const expectedReviewsTitle = 'Отзывы';
+    const reviewItemTestId = 'review-item';
+    const expectedShowMoreButtonText = 'Показать больше отзывов';
     const mockProduct = getMockProduct();
     const mockReviews = Array.from({ length: 5 }, getMockReview);
     const expectedShowedReviewsCount = 3;
@@ -137,18 +172,20 @@ describe('Component: ProductPage', () => {
 
     render(withStoreComponent);
 
-    expect(screen.getByText('Отзывы')).toBeInTheDocument();
-    expect(screen.getAllByTestId('review-item')).toHaveLength(
+    expect(screen.getByText(expectedReviewsTitle)).toBeInTheDocument();
+    expect(screen.getAllByTestId(reviewItemTestId)).toHaveLength(
       expectedShowedReviewsCount
     );
     expect(
-      screen.getByRole('button', {
-        name: /Показать больше отзывов/i,
+      screen.getByRole(ElementRole.Button, {
+        name: expectedShowMoreButtonText,
       })
     ).toBeInTheDocument();
   });
 
   it('should shows modal when add button is clicked', () => {
+    const expectedAddButtonText = 'Добавить в корзину';
+    const expectedAddModalTitleText = 'Добавить товар в корзину';
     const mockProduct = getMockProduct();
     const mockStore = getMockStore({
       [NameSpace.Products]: getMockProductsState({
@@ -162,16 +199,21 @@ describe('Component: ProductPage', () => {
 
     render(withStoreComponent);
 
-    const addButton = screen.getByRole('button', {
-      name: /Добавить в корзину/i,
+    const addButton = screen.getByRole(ElementRole.Button, {
+      name: expectedAddButtonText,
     });
     expect(addButton).toBeInTheDocument();
     fireEvent.click(addButton);
-
-    expect(screen.getByText('Добавить товар в корзину')).toBeInTheDocument();
+    expect(screen.getByText(expectedAddModalTitleText)).toBeInTheDocument();
   });
 
   it('should render up button and scroll to top when is clicked', () => {
+    const upButtonTextId = 'up-btn';
+    const expectedWindowScrollToProps = {
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    };
     const mockProduct = getMockProduct();
     const mockStore = getMockStore({
       [NameSpace.Products]: getMockProductsState({
@@ -185,17 +227,13 @@ describe('Component: ProductPage', () => {
 
     render(withStoreComponent);
 
-    const upButton = screen.getByTestId('up-btn');
+    const upButton = screen.getByTestId(upButtonTextId);
     fireEvent.click(upButton);
-
-    expect(window.scrollTo).toHaveBeenCalledWith({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
+    expect(window.scrollTo).toHaveBeenCalledWith(expectedWindowScrollToProps);
   });
 
   it('should render loader when productDetailsStatus is loadig', () => {
+    const expectedLodingText = 'Loading...';
     const mockProduct = getMockProduct();
     const mockStore = getMockStore({
       [NameSpace.Products]: getMockProductsState({
@@ -208,10 +246,11 @@ describe('Component: ProductPage', () => {
 
     render(withStoreComponent);
 
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    expect(screen.getByText(expectedLodingText)).toBeInTheDocument();
   });
 
   it('should render loader when productDetailsStatus is idle', () => {
+    const expectedLodingText = 'Loading...';
     const mockProduct = getMockProduct();
     const mockStore = getMockStore({
       [NameSpace.Products]: getMockProductsState({
@@ -224,10 +263,11 @@ describe('Component: ProductPage', () => {
 
     render(withStoreComponent);
 
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    expect(screen.getByText(expectedLodingText)).toBeInTheDocument();
   });
 
   it('should render a not found page if there is no product details', () => {
+    const expectedNotFoundPageText = '404: Page Not Found';
     const mockStore = getMockStore({
       [NameSpace.Products]: getMockProductsState({
         productDetailsStatus: RequestStatus.Failed,
@@ -239,10 +279,11 @@ describe('Component: ProductPage', () => {
 
     render(withStoreComponent);
 
-    expect(screen.getByText('404: Page Not Found')).toBeInTheDocument();
+    expect(screen.getByText(expectedNotFoundPageText)).toBeInTheDocument();
   });
 
   it('should dispatch setFilters when specified search parameters', () => {
+    const searchParams = '?tab=description';
     const mockProduct = getMockProduct({ id: 1 });
     const mockState = getMockStore({
       [NameSpace.Products]: getMockProductsState({
@@ -262,7 +303,10 @@ describe('Component: ProductPage', () => {
     );
     const dispatchSpy = vi.spyOn(mockStore, 'dispatch');
     mockBrowserHistory.push(
-      `${AppRoute.Product.replace(':id', '1')}?tab=description`
+      `${AppRoute.Product.replace(
+        ':id',
+        String(mockProduct.id)
+      )}${searchParams}`
     );
 
     render(withStoreComponent);
