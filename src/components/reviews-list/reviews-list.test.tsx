13fab882cfc85293fd6755a016cbd0c285bf7c40
bozhinF <bojin.fedor@gmail.com>
2024-12-29 21:10:18 +0300

    Поправит тесты для компонента ReviewsList

--- src/components/reviews-list/reviews-list.test.tsx
+++ src/components/reviews-list/reviews-list.test.tsx
@@ -1,44 +1,59 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import { Reviews } from '../../types/types';
 import ReviewsList from './reviews-list';
 import { getMockReview } from '../../util/mocks';
+import { ElementRole } from '../../const/const';
 
 describe('Component: ReviewsList', () => {
-  const mockReviews: Reviews = Array.from({ length: 4 }, getMockReview);
+  const mockReviews = Array.from({ length: 4 }, getMockReview);
 
   it('should renders the correct number of reviews initially', () => {
+    const epxectedReviewsText = 'Отзывы';
+    const expectedReviewItemTestId = 'review-item';
+    const expectedReviewItemsCount = 3;
+
     render(<ReviewsList reviews={mockReviews} />);
 
-    expect(screen.getByText('Отзывы')).toBeInTheDocument();
-    expect(screen.getAllByTestId('review-item')).toHaveLength(3);
+    expect(screen.getByText(epxectedReviewsText)).toBeInTheDocument();
+    expect(screen.getAllByTestId(expectedReviewItemTestId)).toHaveLength(
+      expectedReviewItemsCount
+    );
   });
 
   it('should shows more reviews when button is clicked', () => {
+    const expectedShowMoreButtonText = /Показать больше отзывов/i;
+    const expectedReviewItemTestId = 'review-item';
+    const expectedReviewItemsCount = 4;
+
     render(<ReviewsList reviews={mockReviews} />);
 
-    const showMoreButton = screen.getByRole('button', {
-      name: /Показать больше отзывов/i,
+    const showMoreButton = screen.getByRole(ElementRole.Button, {
+      name: expectedShowMoreButtonText,
     });
     fireEvent.click(showMoreButton);
-
-    expect(screen.getAllByTestId('review-item')).toHaveLength(4);
+    expect(screen.getAllByTestId(expectedReviewItemTestId)).toHaveLength(
+      expectedReviewItemsCount
+    );
   });
 
   it('should does not show the button when all reviews are displayed', () => {
+    const expectedShowMoreButtonText = /Показать больше отзывов/i;
+
     render(<ReviewsList reviews={mockReviews} />);
 
-    const showMoreButton = screen.getByRole('button', {
-      name: /Показать больше отзывов/i,
+    const showMoreButton = screen.getByRole(ElementRole.Button, {
+      name: expectedShowMoreButtonText,
     });
     fireEvent.click(showMoreButton);
-
     expect(showMoreButton).not.toBeInTheDocument();
   });
 
   it('should renders no reviews message when there are no reviews', () => {
+    const epxectedReviewsText = 'Отзывы';
+    const expectedReviewItemTestId = 'review-item';
+
     render(<ReviewsList reviews={[]} />);
 
-    expect(screen.getByText('Отзывы')).toBeInTheDocument();
-    expect(screen.queryByTestId('review-item')).toBeNull();
+    expect(screen.getByText(epxectedReviewsText)).toBeInTheDocument();
+    expect(screen.queryByTestId(expectedReviewItemTestId)).toBeNull();
   });
 });
