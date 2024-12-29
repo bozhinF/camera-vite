385e9079a31904ce686e67f6f70c65f15331b358
bozhinF <bojin.fedor@gmail.com>
2024-12-29 22:04:49 +0300

    Поправит тесты для хука useElementListener

--- src/hooks/use-element-listener.test.tsx
+++ src/hooks/use-element-listener.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { useRef } from 'react';
 import userEvent from '@testing-library/user-event';
 import { useElementListener } from './use-element-listener';
+import { ElementRole } from '../const/const';
 
 const mockListener = vi.fn();
 
@@ -19,18 +20,17 @@ const TestComponent = ({ eventName }: TestComponentProps) => {
 
 describe('useElementListener', () => {
   it('should adds event listener on mount and removes it on unmount', () => {
-    const { unmount } = render(<TestComponent eventName="click" />);
-    const button = screen.getByText('Click Me');
+    const eventName = 'click';
+    const buttonText = 'Click Me';
 
+    const { unmount } = render(<TestComponent eventName={eventName} />);
+
+    const button = screen.getByText(buttonText);
     userEvent.click(button);
-
-    expect(screen.getByRole('button')).toBeInTheDocument();
+    expect(screen.getByRole(ElementRole.Button)).toBeInTheDocument();
     expect(vi.isMockFunction(vi.fn())).toBe(true);
-
     unmount();
-
     userEvent.click(button);
-
     expect(mockListener).not.toHaveBeenCalled();
   });
 });
