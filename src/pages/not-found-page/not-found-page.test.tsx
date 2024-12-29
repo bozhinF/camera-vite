c80d3f979dce5e6e537888ab60b19c9507ef504a
bozhinF <bojin.fedor@gmail.com>
2024-12-29 22:32:16 +0300

    Поправит тесты для компонента NotFoundPage

--- src/pages/not-found-page/not-found-page.test.tsx
+++ src/pages/not-found-page/not-found-page.test.tsx
@@ -1,30 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { AppRoute } from '../../const/const';
+import { AppRoute, ElementAttribute, ElementRole } from '../../const/const';
 import NotFoundPage from './not-found-page';
 
 describe('Component: NotFoundPage', () => {
   it('should renders NotFoundPage with 404 message', () => {
+    const expectedHeadingText = '404: Page Not Found';
     render(
       <MemoryRouter>
         <NotFoundPage />
       </MemoryRouter>
     );
 
-    expect(screen.getByRole('heading')).toHaveTextContent(
-      '404: Page Not Found'
+    expect(screen.getByRole(ElementRole.Heading)).toHaveTextContent(
+      expectedHeadingText
     );
   });
 
   it('should renders link to Main page', () => {
+    const expectedToMainPageLinkText = 'Go to Main page';
     render(
       <MemoryRouter>
         <NotFoundPage />
       </MemoryRouter>
     );
 
-    const linkElement = screen.getByRole('link', { name: /Go to Main page/i });
+    const linkElement = screen.getByRole(ElementRole.Link, {
+      name: expectedToMainPageLinkText,
+    });
     expect(linkElement).toBeInTheDocument();
-    expect(linkElement).toHaveAttribute('href', AppRoute.Main);
+    expect(linkElement).toHaveAttribute(ElementAttribute.Href, AppRoute.Main);
   });
 });
