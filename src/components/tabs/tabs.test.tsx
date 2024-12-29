761f65bc5167153a0f848cba259e5fe2d51cb03a
bozhinF <bojin.fedor@gmail.com>
2024-12-29 21:41:58 +0300

    Поправит тесты для компонента Tabs

--- src/components/tabs/tabs.test.tsx
+++ src/components/tabs/tabs.test.tsx
@@ -1,10 +1,9 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import { Product } from '../../types/types';
 import Tabs from './tabs';
 import { getMockFilterState, getMockProduct } from '../../util/mocks';
-import { FilterState } from '../../store/filter-slice/filter-slice';
+import { ElementRole } from '../../const/const';
 
-const mockProduct: Product = getMockProduct({
+const mockProduct = getMockProduct({
   vendorCode: '12345',
   category: 'Камера',
   type: 'Цифровая',
@@ -12,7 +11,7 @@ const mockProduct: Product = getMockProduct({
   description: 'Описание продукта',
 });
 
-const mockFilterState: FilterState = getMockFilterState({
+const mockFilterState = getMockFilterState({
   tab: 'characteristics',
 });
 
@@ -24,6 +23,14 @@ describe('Component: Tabs', () => {
   });
 
   it('should renders tabs and displays the correct content', () => {
+    const expectedCharacteristicsTabText = 'Характеристики';
+    const expectedDescriptionTabText = 'Описание';
+    const activeTabClass = 'is-active';
+    const expectedVendorCodeTitleText = 'Артикул:';
+    const expectedCategoryTitleText = 'Категория:';
+    const expectedTypeTitleText = 'Тип камеры:';
+    const expectedLevelTitleText = 'Уровень:';
+
     render(
       <Tabs
         product={mockProduct}
@@ -32,27 +39,35 @@ describe('Component: Tabs', () => {
       />
     );
 
-    expect(screen.getByText('Характеристики')).toBeInTheDocument();
-    expect(screen.getByText('Описание')).toBeInTheDocument();
-
-    expect(screen.getByRole('button', { name: 'Характеристики' })).toHaveClass(
-      'is-active'
-    );
-    expect(screen.getByRole('button', { name: 'Описание' })).not.toHaveClass(
-      'is-active'
-    );
-
-    expect(screen.getByText('Артикул:')).toBeInTheDocument();
+    expect(
+      screen.getByText(expectedCharacteristicsTabText)
+    ).toBeInTheDocument();
+    expect(screen.getByText(expectedDescriptionTabText)).toBeInTheDocument();
+    expect(
+      screen.getByRole(ElementRole.Button, {
+        name: expectedCharacteristicsTabText,
+      })
+    ).toHaveClass(activeTabClass);
+    expect(
+      screen.getByRole(ElementRole.Button, { name: expectedDescriptionTabText })
+    ).not.toHaveClass(activeTabClass);
+    expect(screen.getByText(expectedVendorCodeTitleText)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.vendorCode)).toBeInTheDocument();
-    expect(screen.getByText('Категория:')).toBeInTheDocument();
+    expect(screen.getByText(expectedCategoryTitleText)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.category)).toBeInTheDocument();
-    expect(screen.getByText('Тип камеры:')).toBeInTheDocument();
+    expect(screen.getByText(expectedTypeTitleText)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.type)).toBeInTheDocument();
-    expect(screen.getByText('Уровень:')).toBeInTheDocument();
+    expect(screen.getByText(expectedLevelTitleText)).toBeInTheDocument();
     expect(screen.getByText(mockProduct.level)).toBeInTheDocument();
   });
 
   it('should changes tab when clicked and calls onChange', () => {
+    const expectedCharacteristicsTabText = 'Характеристики';
+    const expectedDescriptionTabText = 'Описание';
+    const activeTabClass = 'is-active';
+    const expectedUpdateKey = 'tab';
+    const expectedUpdateValue = 'description';
+
     render(
       <Tabs
         product={mockProduct}
@@ -61,23 +76,24 @@ describe('Component: Tabs', () => {
       />
     );
 
-    fireEvent.click(screen.getByText('Описание'));
-
-    expect(screen.getByRole('button', { name: 'Описание' })).toHaveClass(
-      'is-active'
-    );
+    fireEvent.click(screen.getByText(expectedDescriptionTabText));
     expect(
-      screen.getByRole('button', { name: 'Характеристики' })
-    ).not.toHaveClass('is-active');
-
+      screen.getByRole(ElementRole.Button, { name: expectedDescriptionTabText })
+    ).toHaveClass(activeTabClass);
+    expect(
+      screen.getByRole(ElementRole.Button, {
+        name: expectedCharacteristicsTabText,
+      })
+    ).not.toHaveClass(activeTabClass);
     expect(screen.getByText(mockProduct.description)).toBeInTheDocument();
-
     expect(mockOnChange).toHaveBeenCalledWith({ ...mockFilterState }, [
-      { key: 'tab', value: 'description' },
+      { key: expectedUpdateKey, value: expectedUpdateValue },
     ]);
   });
 
   it('should does not change if tab is already active', () => {
+    const expectedCharacteristicsTabText = 'Характеристики';
+
     render(
       <Tabs
         product={mockProduct}
@@ -86,7 +102,7 @@ describe('Component: Tabs', () => {
       />
     );
 
-    fireEvent.click(screen.getByText('Характеристики'));
+    fireEvent.click(screen.getByText(expectedCharacteristicsTabText));
 
     expect(mockOnChange).not.toHaveBeenCalled();
   });
