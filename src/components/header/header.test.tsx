0492a7bedd8353a8c4eaacce28f15fce35d7d75e
bozhinF <bojin.fedor@gmail.com>
2024-12-29 17:11:32 +0300

    Поправит тесты для компонента Header

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import Header from './header';
 import { withHistory, withStore } from '../../util/mock-component';
 import { createMemoryHistory, MemoryHistory } from 'history';
-import { AppRoute } from '../../const/const';
+import { AppRoute, ElementAttribute } from '../../const/const';
 import { getMockStore } from '../../util/mocks';
 
 describe('Component: Header', () => {
@@ -14,6 +14,7 @@ describe('Component: Header', () => {
   });
 
   it('should renders logo link', () => {
+    const expectedLogoLinkLabelText = /переход на главную/i;
     const withHistoryComponent = withHistory(<Header />, mockHistory);
     const { withStoreComponent } = withStore(
       withHistoryComponent,
@@ -23,9 +24,9 @@ describe('Component: Header', () => {
 
     render(withStoreComponent);
 
-    const logoLink = screen.getByLabelText(/переход на главную/i);
+    const logoLink = screen.getByLabelText(expectedLogoLinkLabelText);
     expect(logoLink).toBeInTheDocument();
-    expect(logoLink).toHaveAttribute('href', AppRoute.Main);
+    expect(logoLink).toHaveAttribute(ElementAttribute.Href, AppRoute.Main);
   });
 
   it('should renders navigation links', () => {
@@ -47,11 +48,12 @@ describe('Component: Header', () => {
     links.forEach(({ text, href }) => {
       const navLink: HTMLAnchorElement = screen.getByText(text);
       expect(navLink).toBeInTheDocument();
-      expect(navLink).toHaveAttribute('href', href);
+      expect(navLink).toHaveAttribute(ElementAttribute.Href, href);
     });
   });
 
   it('should renders FormSearch component', () => {
+    const expectedSearchInputPlaceholderText = /поиск по сайту/i;
     const withHistoryComponent = withHistory(<Header />, mockHistory);
     const { withStoreComponent } = withStore(
       withHistoryComponent,
@@ -61,11 +63,14 @@ describe('Component: Header', () => {
 
     render(withStoreComponent);
 
-    const formSearchInput = screen.getByPlaceholderText(/поиск по сайту/i);
+    const formSearchInput = screen.getByPlaceholderText(
+      expectedSearchInputPlaceholderText
+    );
     expect(formSearchInput).toBeInTheDocument();
   });
 
   it('should renders HeaderBasketLink component', () => {
+    const expectedHeaderBasketLinkTestId = 'basket-link';
     const withHistoryComponent = withHistory(<Header />, mockHistory);
     const { withStoreComponent } = withStore(
       withHistoryComponent,
@@ -75,7 +80,7 @@ describe('Component: Header', () => {
 
     render(withStoreComponent);
 
-    const headerBasketLink = screen.getByTestId('basket-link');
+    const headerBasketLink = screen.getByTestId(expectedHeaderBasketLinkTestId);
     expect(headerBasketLink).toBeInTheDocument();
   });
 });
